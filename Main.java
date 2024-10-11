21cff48cf476791789d76ac2d249530726af64dd
techtechtech01 <167213367+techtechtech01@users.noreply.github.com>
2024-10-11 12:03:51 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,10 +29,59 @@ public class Main {
       switch(choice){
         case 1:
           showroom[showroom_counter]=new Showroom();
-            showroom[showroom_counter].set_details();
+               showroom[showroom_counter].set_details();
 showroom_counter++;
           
           System.out.println("1.Add New Showroom");
           System.out.println("9.Back to Main Menu");
           choice=sc.nextInt();
           break;
+        case 2:
+          employee[no_of_employees]=new Employee();
+          employee[no_of_employees].set_details();
+          no_of_employees++;
+           
+          System.out.println("2.Add New Employee");
+          System.out.println("9.Back to Main Menu");
+          choice=sc.nextInt();
+          
+          break;
+        case 3:
+          car[no_of_cars]=new Car();
+          car[no_of_cars].set_details();
+          no_of_cars++;
+          
+          System.out.println("3.Add New Car");
+          System.out.println("9.Back to Main Menu");
+          choice=sc.nextInt();
+          break;
+        case 4:
+          for(int i =0;i<showroom_counter;i++){
+          showroom[i].get_details();
+            }
+          System.out.println();
+          System.out.println();
+System.out.println("9.Back to Main Menu");
+          System.out.println("0.Exit");
+          choice=sc.nextInt();
+          break;
+        case 5:
+          for(int i =0;i<no_of_employees;i++){
+          employee[i].get_details();
+          }
+          System.out.println();
+          System.out.println();
+          System.out.println("9.Back to Main Menu");
+          System.out.println("0.Exit");
+          
+          choice=sc.nextInt();
+          break;
+        case 6:
+          for(int i =0;i<no_of_cars;i++){
+          car[i].get_details();
+            }
+          System.out.println();
+          System.out.println();
+          System.out.println("9.Back to Main Menu");
+          System.out.println("0.Exit");
+       
